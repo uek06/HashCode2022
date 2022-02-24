352545f5eaba1c75caa55e1dabc6514e308f1255
uek06 <ugo.elkoubi@gmail.com>
2022-02-24 18:50:29 +0100

    add in

--- src/com/thales/Main.java
+++ src/com/thales/Main.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 
 public class Main {
-    private static final String[] fileNames = {Filename.EXAMPLE_1.name, Filename.EXAMPLE_2.name, Filename.EXAMPLE_3.name, Filename.EXAMPLE_4.name, Filename.EXAMPLE_5.name};
+    private static final String[] fileNames = {Filename.EXAMPLE_1.name, Filename.EXAMPLE_2.name, Filename.EXAMPLE_3.name, Filename.EXAMPLE_4.name, Filename.EXAMPLE_5.name, Filename.EXAMPLE_6.name};
     private static int test;
 
     public static void main(String[] args) {
--- src/com/thales/models/Filename.java
+++ src/com/thales/models/Filename.java
@@ -1,11 +1,12 @@
 package com.thales.models;
 
 public enum Filename {
-    EXAMPLE_1("a_example"),
-    EXAMPLE_2("b_should_be_easy"),
-    EXAMPLE_3("c_no_hurry"),
-    EXAMPLE_4("d_metropolis"),
-    EXAMPLE_5("e_high_bonus");
+    EXAMPLE_1("a_an_example"),
+    EXAMPLE_2("b_better_start_small"),
+    EXAMPLE_3("c_collaboration"),
+    EXAMPLE_4("d_dense_schedule"),
+    EXAMPLE_5("e_exceptional_skills"),
+    EXAMPLE_6("f_find_great_mentors");
 
     public final String name;
 
